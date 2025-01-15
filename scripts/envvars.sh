be10953d12343bdbf270e564ddae863bcba2efec
Daniele Muffato <danixgithub1@gmail.com>
2025-01-15 21:03:14 +0100

    updated scripts

--- scripts/envvars.sh
+++ scripts/envvars.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
-DIR_ROOT="$(realpath "$(dirname "${SCRIPT_PWD}")/..")"
-DIR_RICETTE="$(realpath "$DIR_ROOT"/ricette)"
-
-export DIR_ROOT DIR_RICETTE
-
-# COPY AND PASTE THE FOLLOWING LINE TO USE THIS SCRIPT!
+##################################################################
+###### COPY AND PASTE THE FOLLOWING LINE TO USE THIS SCRIPT ######
 # source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/envvars.sh"
+##################################################################
+
+# PATHS
+export DIR_ROOT="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")"
+export DIR_RICETTE="$(realpath "$DIR_ROOT"/ricette)"
+
+# SYMBOLS
+export SYMBOL_CORRECT="✔"
+export SYMBOL_WRONG=""
--- scripts/list-todo.sh
+++ scripts/list-todo.sh
@@ -4,9 +4,13 @@ source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/envvars.sh"
 
 for ricetta in "$DIR_RICETTE/"*.md; do
     ricetta="$(realpath "$ricetta")"
+    ricetta_name="$(basename "$ricetta")"
     OUTPUT="$(grep -n TODO "$ricetta")"
+    TODOS="$(echo "$OUTPUT" | wc -l)"
     if [[ -n $OUTPUT ]]; then
-        echo -e "\e[1;33m$ricetta\e[m"
+        echo -e "\e[1;31m- $ricetta_name: $TODOS $SYMBOL_WRONG\e[m"
         echo "$OUTPUT"
+    else
+        echo -e "\e[1;32m- $ricetta_name: $TODOS $SYMBOL_CORRECT\e[m"
     fi
 done
