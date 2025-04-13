2b12de202bcf44b1dc06c829765bd7a453b6f2ef
Daniele Muffato <danixgithub1@gmail.com>
2025-04-13 11:33:02 +0200

    fixed batcat not working (only bat before)

--- scripts/list-todo.sh
+++ scripts/list-todo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if command -v bat &>/dev/null; then
+    VIEWER='bat'
+elif command -v batcat &>/dev/null; then
+    VIEWER='batcat'
+else
+    echo 'no file viewer command found! Try installing bat...'
+    exit 1
+fi
+
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.envvars.sh"
 
 for ricetta in "$DIR_RICETTE/"*.md; do
@@ -9,7 +18,7 @@ for ricetta in "$DIR_RICETTE/"*.md; do
     TODOS="$(echo "$OUTPUT" | wc -l)"
     if [[ -n $OUTPUT ]]; then
         echo -e "\e[1;31m- $ricetta_name: $TODOS $SYMBOL_WRONG\e[m"
-        bat "$ricetta" --color=always $(grep -n TODO "$ricetta" | cut -d: -f1 | while read -r line; do echo --highlight-line="$line"; done)
+        "$VIEWER" "$ricetta" --color=always "$(grep -n TODO "$ricetta" | cut -d: -f1 | while read -r line; do echo --highlight-line="$line"; done)"
     else
         echo -e "\e[1;32m- $ricetta_name: 0 $SYMBOL_CORRECT\e[m"
     fi
