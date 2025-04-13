b3abb85bdf9fd0e6581545db867bb5d327a73c46
Daniele Muffato <danixgithub1@gmail.com>
2025-04-13 11:31:04 +0200

    file viewer finder

--- scripts/open-ricetta.sh
+++ scripts/open-ricetta.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+VIEWER=""
+if command -v bat &>/dev/null; then
+    VIEWER='bat'
+elif command -v batcat &>/dev/null; then
+    VIEWER='batcat'
+elif command -v cat &>/dev/null; then
+    VIEWER='cat'
+else
+    echo 'no file viewer command found! Try installing cat or bat...'
+    exit 1
+fi
+
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.envvars.sh"
 
 cd "${DIR_RICETTE}" || exit 1
-bat "$(fzf --height=40% --layout=reverse --border)"
+"$VIEWER" "$(fzf --height=40% --layout=reverse --border)"
